70cce62e05cfb0d1f4f30d5db4e6f2794a11695b
pikacoder44 <pikaop125@gmail.com>
2026-08-13 19:52:04 +0500

    Added register User route

---
+++ app/api/auth/register/register.ts
@@ -0,0 +1,61 @@
+import { NextResponse } from "next/server";
+import User from "../../../models/User";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: Request) {
+  try {
+    // Extract userData from the incoming request body
+    const userData = await request.json();
+
+    const { username, password, confirmPassword } = userData;
+    const validationErrors: string[] = [];
+
+    if (!username || username.trim().length < 3) {
+      validationErrors.push("Username must be at least 3 characters long.");
+    }
+
+    if (!password || password.trim().length < 8) {
+      validationErrors.push("Password must be at least 8 characters long.");
+    }
+
+    if (password !== confirmPassword) {
+      validationErrors.push("Passwords do not match.");
+    }
+
+    if (validationErrors.length > 0) {
+      return NextResponse.json({ errors: validationErrors }, { status: 400 });
+    }
+
+    // Check if user already exists in the database
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return NextResponse.json(
+        { errors: ["Username already exists."] },
+        { status: 400 },
+      );
+    }
+
+    // Hash the password before saving to the database
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Save the new user to the database
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    // return a success response
+    return NextResponse.json(
+      { message: "User registered successfully!" },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Error in POST /api/auth/register:", error);
+    return NextResponse.json(
+      { errors: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
