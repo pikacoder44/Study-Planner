1bf3ebbf180e9faad7374a150552e18c159c6b20
pikacoder44 <pikaop125@gmail.com>
2026-08-16 10:05:14 +0500

    Added StudySession model

---
+++ models/StudySession.ts
@@ -0,0 +1,56 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ISubjectSession extends Document {
+  userId: string;
+  subjectId: string;
+  title: string;
+  startTime: Date;
+  endTime: Date;
+  duration: number;
+  notes?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const StudySessionSchema: Schema<ISubject> = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    subjectId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    notes: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const StudySession =
+  mongoose.models.StudySession ||
+  mongoose.model<ISubjectSession>("StudySession", StudySessionSchema);
+
+export default StudySession;
