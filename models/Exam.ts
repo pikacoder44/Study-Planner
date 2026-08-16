addcc495aa7a29816d5dc450d1e9322f96a7d4ce
pikacoder44 <pikaop125@gmail.com>
2026-08-16 10:09:24 +0500

    Added Exam model

---
+++ models/Exam.ts
@@ -0,0 +1,61 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IExam extends Document {
+  userId: string;
+  subjectId: string;
+  title: string;
+  examDate: Date;
+  startTime: Date;
+  endTime?: Date;
+  location?: string;
+  description?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ExamSchema: Schema<IExam> = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    subjectId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    examDate: {
+      type: Date,
+      required: true,
+    },
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+      required: false,
+    },
+    location: {
+      type: String,
+      required: false,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Exam =
+  mongoose.models.Exam ||
+  mongoose.model<IExam>("Exam", ExamSchema);
+
+export default Exam;
