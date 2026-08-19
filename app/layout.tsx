668058e6589c0ed9a2f5c84b2d1b39d09b51acd3
pikacoder44 <pikaop125@gmail.com>
2026-08-19 12:52:06 +0500

    Fixed hydration warning by placing Navbar component inside body

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Navbar />
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
