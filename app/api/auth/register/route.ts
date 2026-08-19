cc55b85b54af6f2d288c41a04c910cec1112cfd2
pikacoder44 <pikaop125@gmail.com>
2026-08-19 11:47:51 +0500

    Updated Model import

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import User from "../../../models/User";
+import User from "../../../../models/User";
 import bcrypt from "bcryptjs";
 import { signToken } from "@/lib/jwt";
 export async function POST(request: Request) {
